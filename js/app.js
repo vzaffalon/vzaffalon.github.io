c12b3f72fdfa12332e48e8afe351f4423a170746
vzaffalon <zaffalonvictor@gmail.com>
2017-03-20 03:14:49 -0300

    particles.js css changes github and links

---
+++ js/app.js
@@ -0,0 +1,52 @@
+var partJson = {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+    },
+    "opacity": {
+      "value": 1,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2,
+      "random": false,
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+    }
+  },
+  "retina_detect": true
+}
+var jsonUri = "data:text/plain;base64,"+window.btoa(JSON.stringify(partJson));
+
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', jsonUri, function() {
+  console.log('callback - particles.js config loaded');
+});
