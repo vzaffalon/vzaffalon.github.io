4f1b2dac5ebe9453ded3a4d47811385199bf00a0
vzaffalon <zaffalonvictor@gmail.com>
2017-03-20 23:39:43 -0300

    test

--- js/particles.js
+++ js/particles.js
@@ -1527,6 +1527,7 @@ window.particlesJS.load = function(tag_id, path_config_json, callback){
   xhr.onreadystatechange = function (data) {
     if(xhr.readyState == 4){
       if(xhr.status == 200){
+        debugger;
         var params = JSON.parse(data.currentTarget.responseText);
         window.particlesJS(tag_id, params);
         if(callback) callback();
